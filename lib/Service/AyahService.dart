71297fce6f340b183041f0821f63ca0a2dcfa50b
mohamedmagdy2301 <mohammedmego15@gmail.com>
2024-02-22 20:42:20 +0200

    scaend commit

--- lib/Service/AyahService.dart
+++ lib/Service/AyahService.dart
@@ -4,11 +4,12 @@ import 'package:quran/Service/Api.dart';
 import 'package:quran/models/AyahModel.dart';
 
 class AyahService {
-  Future<List<AyahModel>> getAyahData(int suraNumber,String nameReader) async {
+  Future<List<AyahModel>> getAyahData(int suraNumber, String nameReader) async {
     Map<String, dynamic> AyahList = await Api().getApi(
         url: 'https://api.alquran.cloud/v1/surah/$suraNumber/ar.$nameReader');
     List<AyahModel> ayahModelList = [];
-    // Map<String, dynamic> AyahModelListFinal = {};
+    // Map<String, dynamic> AyahModelLis    //tFinal = {};
+    // mnmnmn
     for (int i = 0; i < AyahList['data']['ayahs'].length; i++) {
       ayahModelList.add(AyahModel.fromJson(AyahList['data']['ayahs'][i]));
     }
